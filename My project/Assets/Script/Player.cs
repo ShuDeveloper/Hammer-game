ae5b3b18694796f26b74ed9e15275d7f55ece456
Shubham Gautam <138363357+ShuDeveloper@users.noreply.github.com>
2024-01-02 20:04:41 +0530

    input and move player

--- My project/Assets/Script/Player.cs
+++ My project/Assets/Script/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector3 PlayerPostion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPostion = transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -67.529f);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            transform.rotation = Quaternion.Euler(transform.position.x, 0, 0);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            PlayerPostion = new Vector3(transform.position.x,3.4f,0);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            PlayerPostion = new Vector3(transform.position.x, -4, 0); 
+        }
+        transform.position = PlayerPostion;
     }
 }
